7052a7eb4bacd4a95ab6c72aea19069373e8d063
hashim222 <aslamhashim19@gmail.com>
2022-08-16 15:50:38 +0000

    ...

--- todo/forms.py
+++ todo/forms.py
@@ -5,4 +5,7 @@ from .models import Item
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ['name', 'done']
+        fields = {
+            'name',
+            'done'
+        }
